66048f81a011d05ef8b21de98499092fd4f7733e
Dmitriy Belov <wassermannsn@gmail.com>
2022-04-27 16:30:40 +0600

    handling form submossion

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const formElement = document.querySelector('form');
 
 
 const playerConfigOverlay = document.getElementById('config-overlay');
@@ -12,3 +13,5 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
 cancelBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,21 @@
+// открытие оверлея и появление заднего фона по клику на кнопку
 function openPlayerConfig () {
     playerConfigOverlay.style.display = 'block';
     backdropElement.style.display = 'block';
 }
-
+// закрытие оверлея по клику на кнопку или заднему фону
 function closePlayerConfig() {
     playerConfigOverlay.style.display = 'none';
     backdropElement.style.display = 'none';
+}
+
+function savePlayerConfig(event) {
+    // убрать дефолт состояние объекта
+    event.preventDafault();
+    // создать новый экземпляр объекта с сылкой на объект события
+    const formData = new FormData(event.target);
+    // получить поле с которого нужно считать данные
+    const enteredPlayerName = formData.get('playername');
+   
+
 }
