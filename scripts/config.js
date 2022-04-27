7cb9843405785eac98e4ad05203388c54eb10f75
Dmitriy Belov <wassermannsn@gmail.com>
2022-04-27 17:16:48 +0600

    validating user input

--- scripts/config.js
+++ scripts/config.js
@@ -1,21 +1,24 @@
 // открытие оверлея и появление заднего фона по клику на кнопку
-function openPlayerConfig () {
-    playerConfigOverlay.style.display = 'block';
-    backdropElement.style.display = 'block';
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
 }
 // закрытие оверлея по клику на кнопку или заднему фону
 function closePlayerConfig() {
-    playerConfigOverlay.style.display = 'none';
-    backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
-    // убрать дефолт состояние объекта
-    event.preventDafault();
-    // создать новый экземпляр объекта с сылкой на объект события
-    const formData = new FormData(event.target);
-    // получить поле с которого нужно считать данные
-    const enteredPlayerName = formData.get('playername');
-   
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
 
+  if (!enteredPlayername) { // enteredPlayername === ''
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
